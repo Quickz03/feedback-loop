3cba24c802072cb4008255a8079e17bddc543daf
Brian Zilka <Bzilks@gmail.com>
2019-04-05 17:10:09 -0500

    components made, wireframe made

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -5,6 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Feedback!</h1>
@@ -14,16 +15,13 @@ class App extends Component {
             <h1>How are you feeling today?</h1>
             <form onSubmit={this.handleSubmit}>
             <label>feeling?</label>
-            <br/>
            <input type="number" name="feeling" 
                onChange={this.handleChangeFor}></input>
-                <button>Next</button>
             </form>
           </section>
         <br/>
       </div>
+    
     );
   }
 }
-
-export default App;
